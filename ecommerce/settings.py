3f4a002eebdd27ffb5209096108d3038b046d9d3
Mukul Chauhan <chauhanmukul915@gmail.com>
2026-09-05 22:02:32 +0530

    Update Chauhan Store

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -76,10 +76,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=bool(os.getenv("DATABASE_URL")),
+    )
 }
 
 
